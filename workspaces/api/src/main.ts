51b74a7db6e1491e559fa6e08e317933d4093ef9
aaronmartin <aaronjmartin648@gmail.com>
2022-11-06 22:42:57 +0000

    api server init

---
+++ workspaces/api/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./modules/application/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8000);
+}
+bootstrap();
