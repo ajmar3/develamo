f1f9b4b88cd81f186d0e14a45acce2ae4f9e3044
aaronmartin <aaronjmartin648@gmail.com>
2022-11-07 03:02:49 +0000

    enable validation

--- workspaces/api/src/main.ts
+++ workspaces/api/src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/application/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: process.env.CORS_ORIGIN });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
 }
 bootstrap();
